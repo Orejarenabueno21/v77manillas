fee5ff234af425296054ef91fd0426833363f095
raul <orejarena1999@gmail.com>
2023-05-31 15:12:33 -0500

    cuarto commit

--- src/componentes/Producto.jsx
+++ src/componentes/Producto.jsx
@@ -48,6 +48,24 @@ const Producto = () => {
 <option value="opcion4">Martillo</option>
 <option value="opcion1">Ancla</option>
 </select>
+
+  <label for="menu"><h3>
+    Selecciona el tipo de manilla:</h3></label>
+<select id="menu" className='opt'>
+<option value="opcion4">Oro</option>
+<option value="opcion1">Oro rosado</option>
+<option value="opcion1">Plata</option>
+<option value="opcion1">Niquel</option>
+</select>
+
+<h2 for="numero">Ingrese numero de manillas a comprar</h2>
+  <input type="number" id="numero" name="numero"/>
+  <label for="menu"><h3>
+    Selecciona la moneda de compra:</h3></label>
+<select id="menu" className='opt'>
+<option value="opcion4">Peso</option>
+<option value="opcion1">Dolar</option>
+</select>
             <ul className="list-group">
                     {  
                         dijes.map(item =>(
