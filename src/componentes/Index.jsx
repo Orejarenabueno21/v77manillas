afffb36ef6e978bd2b5f633b64efe0b543c43af9
raul <orejarena1999@gmail.com>
2023-05-31 20:04:34 -0500

    quiinto commit

--- src/componentes/Index.jsx
+++ src/componentes/Index.jsx
@@ -1,30 +1,63 @@
-import React from 'react'
-import Producto from './Producto'
+import React, {useEffect, useState} from 'react'
+import {db} from '../firebase'
+import {collection, doc, onSnapshot, query} from 'firebase/firestore'
+
+
+
+
 const Index = () => {
-  return (
- 
+
+ const [dijes,setDijes] = useState([]);
+
+    useEffect(() => {
+
+    const obtenerDijes = async() => {
+
+    try{
+     await onSnapshot(collection(db, 'manillas'), (query) =>{
+
+    setDijes(query.docs.map((doc) => ({...doc.data(), id:doc.id})))
+    })
+
+     }catch(e){
+     console.log(e)
+    }
+
+     }
+
+    obtenerDijes();
+    console.log(obtenerDijes)
+
+     }, [])
+
+  return (
     <div>
-        <body>
-            
-      
- 
-<h1 className='titulo'>MANILLAS DONDE RAUL</h1>
+            <ul className="list-group">
+                    {  
+                        dijes.map(item =>(
+                            <div className="list-group-item" key={item.id}>
+                                <h1 className="lead">{item.Material}-{item.tipo}</h1>
+                            </div>
+                        ))   
+                    }        
+                </ul>
 
- 
 
-  <label for="menu"><h3>
+                {/* 
+ <label for="menu"><h3>
     Selecciona la clase de manilla:</h3></label>
 <select id="menu" className='opt'>
 <option value="opcion4">Cuero</option>
 <option value="opcion1">Cuerda</option>
 </select>
 
-  <label for="menu"><h3>
+ <label for="menu"><h3>
     Selecciona el dije:</h3></label>
-<select id="menu" className='opt'>
-<option value="opcion4">Martillo</option>
+<select id="dije" className='opt' onChange={(e) => setOpcionSeleccionada(e.target.value)}>
+<option value="Material">Martillo</option>
 <option value="opcion1">Ancla</option>
 </select>
+
   <label for="menu"><h3>
     Selecciona el tipo de manilla:</h3></label>
 <select id="menu" className='opt'>
@@ -38,17 +71,18 @@ const Index = () => {
   <input type="number" id="numero" name="numero"/>
   <label for="menu"><h3>
     Selecciona la moneda de compra:</h3></label>
-<select id="menu" className='opt'>
+<select id="menu moneda" className='opt'>
 <option value="opcion4">Peso</option>
 <option value="opcion1">Dolar</option>
-</select>
-  
+</select> */}
 
-</body>
-  
- 
-    </div>
-  )
+
+ </div>
+ )
+
 }
+
+
+
 
 export default Index
--- src/componentes/Producto.jsx
+++ src/componentes/Producto.jsx
@@ -6,76 +6,88 @@ import {collection, doc, onSnapshot, query} from 'firebase/firestore'
 
 
 const Producto = () => {
+    const [dijes,setDijes] = useState([]);
+    const [materialSeleccionado, setMaterialSeleccionado] = useState('');
+    const [dijeSeleccionado, setDijeSeleccionado] = useState('');
+    const [tipoSeleccionado, setTipoSeleccionado] = useState('');
+    const [opcionesSeleccionadas, setOpcionesSeleccionadas] = useState('');
+    const [pagoSelec, setPagoSelec] = useState('');
+    const [opcioncp, setOpcioncp] = useState('');
 
- const [dijes,setDijes] = useState([]);
 
-    useEffect(() => {
+        useEffect(() => {
+            const obtenerDijes = async() => {
+                try{ await onSnapshot(collection(db, 'manillas'), (query) =>{
+                        setDijes(query.docs.map((doc) => ({...doc.data(), id:doc.id})))
+                    })
+                }catch(e){
+                console.log(e)
+                }
+            }
+            obtenerDijes();
+            console.log(obtenerDijes)
+         }, [])
 
-    const obtenerDijes = async() => {
+         
 
-    try{
-     await onSnapshot(collection(db, 'manillas'), (query) =>{
 
-    setDijes(query.docs.map((doc) => ({...doc.data(), id:doc.id})))
-    })
+         useEffect(() => {
+            const opciones = `${materialSeleccionado} ${dijeSeleccionado} ${tipoSeleccionado} ${pagoSelec}`;
+            setOpcionesSeleccionadas(opciones);
+          }, [materialSeleccionado, dijeSeleccionado, tipoSeleccionado, pagoSelec]);
 
-     }catch(e){
-     console.log(e)
-    }
+          useEffect(() => {
+            const opc = `${materialSeleccionado} ${tipoSeleccionado}`;
+            setOpcioncp(opc);
+          }, [materialSeleccionado, tipoSeleccionado]);
 
-     }
-
-    obtenerDijes();
-    console.log(obtenerDijes)
-
-     }, [])
-
-  return (
+ return (
     <div>
-
 <h1 className='titulo'>MANILLAS DONDE RAUL</h1>
+<hr/> 
 
-  <label for="menu"><h3>
+<label for="menu"><h3>
     Selecciona la clase de manilla:</h3></label>
-<select id="menu" className='opt'>
-<option value="opcion4">Cuero</option>
-<option value="opcion1">Cuerda</option>
+    <select id="mate" className='opt' onChange={(e) => setMaterialSeleccionado(e.target.value)}>
+        <option value="c">Cuero</option>
+<option value="k">Cuerda</option>
 </select>
 
-  <label for="menu"><h3>
+ <label for="menu"><h3>
     Selecciona el dije:</h3></label>
-<select id="menu" className='opt'>
-<option value="opcion4">Martillo</option>
-<option value="opcion1">Ancla</option>
+<select id="dije" className='opt' onChange={(e) => setDijeSeleccionado(e.target.value)}>
+<option value="m">Martillo</option>
+<option value="a">Ancla</option>
 </select>
 
   <label for="menu"><h3>
     Selecciona el tipo de manilla:</h3></label>
-<select id="menu" className='opt'>
-<option value="opcion4">Oro</option>
-<option value="opcion1">Oro rosado</option>
-<option value="opcion1">Plata</option>
-<option value="opcion1">Niquel</option>
+    <select id="dije" className='opt' onChange={(e) => setOpcionSeleccionada(e.target.value)}>
+<option value="o">Oro</option>
+<option value="r">Oro rosado</option>
+<option value="p">Plata</option>
+<option value="n">Niquel</option>
 </select>
 
-<h2 for="numero">Ingrese numero de manillas a comprar</h2>
-  <input type="number" id="numero" name="numero"/>
-  <label for="menu"><h3>
-    Selecciona la moneda de compra:</h3></label>
-<select id="menu" className='opt'>
-<option value="opcion4">Peso</option>
-<option value="opcion1">Dolar</option>
-</select>
+            <hr/>
+            <label for="pago">Metodo de pago:</label>
+            <select id="pago" className='opt' onChange={(e) => setPagoSelec(e.target.value)}>
+            <option value="d">Dolares</option>
+            <option value="p">Pesos</option>
+            </select>
+
             <ul className="list-group">
                     {  
                         dijes.map(item =>(
                             <div className="list-group-item" key={item.id}>
-                                <h1 className="lead">{item.Material}-{item.tipo}</h1>
+                                {item.id}
+                                <h1 className="lead">{item[opcionesSeleccionadas]}</h1>
                             </div>
                         ))   
                     }        
-                </ul>
-
+            </ul> 
+ 
+                
  </div>
  )
 
